1b78af6ba304274c192b63f5c17b34838d22f4a6
Mary Czapkowska <43907321+MaryJ25@users.noreply.github.com>
2021-03-08 13:01:07 +0200

    Update and rename main.py to scripts/tesla.py

--- scripts/tesla.py
+++ scripts/tesla.py
@@ -53,12 +53,3 @@ class Tesla:
             return self.check_battery_level
         else:
             return "Battery charge level is 0%, please charge!"
-
-
-class ModelX(Tesla):
-    """Creates the ModelX subclass of Tesla. Model is set to X, colour needs to be given when called."""
-    def __init__(self, color: str, autopilot: bool = False):
-        super().__init__("X", color, autopilot, 0.125)
-
-    def open_doors(self):
-        return "Doors open towards the roof"
